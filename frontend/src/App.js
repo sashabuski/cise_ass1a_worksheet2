b9ea4bde904f343377d79d2544e84682f94b566e
Sasha <sashabuskin12@gmail.com>
2021-07-27 15:27:19 +1200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+//import logo from './logo.svg';
+import './App.css';
+
+const App = () => 
+  
+    <div className="App">
+    <h1>Welcome to CISE – the home of learning and fun</h1> 
+    </div>
+  
+
+export default App;
